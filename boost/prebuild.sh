3ab87f8c9bc4b6baa28ffde3ba7fcd338b825525
gaoqiankun <gqk007@gmail.com>
2016-07-02 09:51:11 +0000

    + boost

---
+++ boost/prebuild.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+dir=$(cd `dirname $0`; pwd)
+builddir="$dir/build"
+
+rm -rf "$builddir"  && cd "$dir/boost"
+./bootstrap.sh --with-libraries=all --with-python=python --prefix="$builddir"
+./b2 --prefix="$builddir" install
+
+for file in lib{32,64}_{debug,release}; do
+  echo "$dir/$file"
+  rm -rf "$dir/$file" && ln -sf "$builddir/lib" "$dir/$file"
+done
+
+ln -sf $builddir/include/boost ${dir}/include
+
