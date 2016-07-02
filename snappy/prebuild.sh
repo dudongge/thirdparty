4117080b4d30eec64d12733e0816e1dbf266a13f
gaoqiankun <gqk007@gmail.com>
2016-07-02 10:10:21 +0000

    + snappy

---
+++ snappy/prebuild.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+dir=$(cd `dirname $0`; pwd)
+builddir="$dir/build"
+
+rm -rf "$builddir"  && cd "$dir/snappy"
+./configure --prefix="$builddir" && make && make install
+
+for file in lib{32,64}_{debug,release}; do
+  echo "$dir/$file"
+  rm -rf "$dir/$file" && ln -sf "$builddir/lib" "$dir/$file"
+done
+
+rm -f $dir/include && ln -sf $builddir/include ${dir}/include
